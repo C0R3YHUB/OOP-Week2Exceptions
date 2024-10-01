8a347bbf62ea417dae55e0abc2ae0290328d6f96
csf24huc <csf24huc@MF124PC16>
2024-10-01 09:54:41 +0100

    Add project files.

---
+++ OOPWEEK2EXCEPTIONS/Program.cs
@@ -0,0 +1,54 @@
+﻿CalculatorApp();
+
+void CalculatorApp()
+{
+    try
+    {
+
+
+
+        Console.WriteLine("Please enter the first number: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("Please enter the second number: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please select a function: +, -, /, * ");
+        char operation = Convert.ToChar(Console.ReadLine());
+        int result = 0;
+
+
+        switch (operation)
+        {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '/':
+                result = firstNumber / secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            default:
+                Console.WriteLine("Invalid Operation.");
+                return;
+
+
+        }
+
+        Console.WriteLine($"Result: {result}");
+
+    }
+    catch (Exception ex)
+    {
+
+    }
+    finally
+    {
+
+    }
+}
